4e067869ec935e662ab795cfc00cdc697a981e2c
chsohn15 <chsohn15@gmail.com>
2020-08-26 14:56:44 -0400

    Completed challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('%c HI', 'color: firebrick')
+//
+
+document.addEventListener("DOMContentLoaded",function(){
+    console.log("Hello World")
+
+    function addImageTag(dog){
+        const imgContainer = document.getElementById('dog-image-container')
+        let imgTag = document.createElement('IMG')
+        imgTag.src = dog
+        imgContainer.appendChild(imgTag)
+    }
+
+    function pictureLoop(dogs){
+        for(const element of dogs){
+            addImageTag(element)
+        }
+        // dogs.forEach(function(dog){ 
+        //     addImageTag(dog)
+        // }
+        // )
+    }
+
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(res => res.json())
+    .then(pictures => pictureLoop(pictures.message)
+    )
+
+    // add image elements to DOm for each image in array
+    
+    
+}
+)
