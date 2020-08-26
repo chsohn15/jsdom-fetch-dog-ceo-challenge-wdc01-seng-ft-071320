55f84b995ada47c7031d9dd0e8f8edf8132129dc
solomonfy <fantahunsolomon@gmail.com>
2020-08-26 16:14:56 -0400

    second challenge done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 //
 
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function () {
     console.log("Hello World")
 
-    function addImageTag(dog){
+    function addImageTag(dog) {
         const imgContainer = document.getElementById('dog-image-container')
         let imgTag = document.createElement('IMG')
         imgTag.src = dog
         imgContainer.appendChild(imgTag)
     }
 
-    function pictureLoop(dogs){
-        for(const element of dogs){
+    function pictureLoop(dogs) {
+        for (const element of dogs) {
             addImageTag(element)
         }
         // dogs.forEach(function(dog){ 
@@ -21,12 +21,38 @@ document.addEventListener("DOMContentLoaded",function(){
     }
 
     fetch("https://dog.ceo/api/breeds/image/random/4")
-    .then(res => res.json())
-    .then(pictures => pictureLoop(pictures.message)
-    )
+        .then(res => res.json())
+        .then(pictures => pictureLoop(pictures.message))
 
     // add image elements to DOm for each image in array
-    
-    
-}
-)
+
+
+    function addLiTag(element) {
+        const dogBreed = document.getElementById("dog-breeds")
+        const li = document.createElement('li');
+        li.id = element
+        li.innerText = element;
+        dogBreed.append(li);
+    }
+
+
+    function addBreed(breeds) {
+        for (let element in breeds) {
+            addLiTag(element)
+            if (breeds[element].length != 0) {
+                let ul = document.createElement('ul');
+                for (const subBreed of breeds[element]) {
+                    let li = document.createElement('li')
+                    li.innerText = subBreed
+                    ul.appendChild(li);
+                    document.getElementById(element).appendChild(ul);
+                }
+            }
+        }
+    }
+
+    fetch("https://dog.ceo/api/breeds/list/all")
+        .then(resp => resp.json())
+        .then(breeds => addBreed(breeds.message))
+
+})
